99e644fe988162dad51eb8d938fa9262c6c13d5b
Lok3rs <lok3rsss@gmail.com>
2020-10-23 20:20:49 +0200

    add animation on main menu

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,16 +1,13 @@
 import time
 import os
 
-from prints import print_board, print_result
+from prints import print_board, print_result, display_tictactoe_animation
 from checks import has_won, is_full
 from moves import get_ai_move, get_move, mark
 
 
 def init_board() -> list:
     return [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
-
-def clear() -> None:
-    return os.system("clear || cls")
 
 def tictactoe_game(human_ai = False, ai_ai = False):
     game_board = init_board()
@@ -19,7 +16,7 @@ def tictactoe_game(human_ai = False, ai_ai = False):
     end_condition = False
 
     while not end_condition:
-        clear()
+        os.system("clear || cls")
         print_board(game_board)
         print("Player X turn:")
         if ai_ai:
@@ -31,7 +28,7 @@ def tictactoe_game(human_ai = False, ai_ai = False):
         if has_won(game_board) or is_full(game_board):
             end_condition = True
             continue
-        clear()
+        os.system("clear || cls")
         print_board(game_board)
         print("Player O turn")
         if human_ai:
@@ -46,15 +43,16 @@ def tictactoe_game(human_ai = False, ai_ai = False):
         if has_won(game_board) or is_full(game_board):
             end_condition = True
 
-    clear()
+    os.system("clear || cls")
     print_result(game_board)
 
 def main_menu():
-    clear()
+    os.system("clear || cls")
+    display_tictactoe_animation()
     user_choose = input("Choose game option:\n 1 - Player vs. Player\n 2 - Player vs. AI\n 3 - AI vs AI\n")
     while user_choose not in ["1", "2", "3"]:
         user_choose = input("Invalid choose. Try again.")
-    clear()
+    os.system("clear || cls")
     if user_choose == "1":
         tictactoe_game()
     elif user_choose == "2":
